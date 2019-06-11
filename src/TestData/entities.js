c489f5e8d00deb6edb9f7885c5278b404eb11c24
licebeam <dustinwalkerart@gmail.com>
2019-06-11 18:36:39 +0900

    adjusts play area for testing

--- src/TestData/entities.js
+++ src/TestData/entities.js
@@ -1,5 +1,5 @@
 import { MAX_WORLD_WIDTH, MAX_WORLD_HEIGHT } from '../constants/constants';
 export const entities = [
-  { id: MAX_WORLD_HEIGHT / 4, sprite: 'player', char: '@' },
-  { id: MAX_WORLD_HEIGHT / 4 + 1, sprite: 'apple', char: 'o' },
+  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2), sprite: 'player', char: '@' },
+  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 2, sprite: 'apple', char: 'o' },
 ]
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const MAX_WORLD_WIDTH = 20;
-export const MAX_WORLD_HEIGHT = MAX_WORLD_WIDTH * 20;
+export const MAX_WORLD_WIDTH = 12;
+export const MAX_WORLD_HEIGHT = MAX_WORLD_WIDTH * 12;
 export const TILE_SIZE = 32;
