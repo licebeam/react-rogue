f4f584d74585d890f769e2b03eb04e7fe7a0a7d6
licebeam <dustinwalkerart@gmail.com>
2019-06-11 18:33:25 +0900

    adusts world size

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const MAX_WORLD_WIDTH = 30;
-export const MAX_WORLD_HEIGHT = MAX_WORLD_WIDTH * 10;
+export const MAX_WORLD_WIDTH = 20;
+export const MAX_WORLD_HEIGHT = MAX_WORLD_WIDTH * 20;
 export const TILE_SIZE = 32;
