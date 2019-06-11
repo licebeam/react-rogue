83798d1b109c12cc585a57025fa7fe8203dccee0
licebeam <dustinwalkerart@gmail.com>
2019-06-11 17:53:27 +0900

    modularizes out styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import styled from 'styled-components';
 import { entities } from './TestData/entities';
+import { TileContainer, Tile, Entity } from './components/styled';
 import { tileGenerator } from './helpers/tileFunctions';
 import { changePlayerPosition } from './helpers/moveFunctions';
-const TileContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(10, 50px);
-`
 
-const Tile = styled.div`
-  height: 50px;
-  width: 50px;
-  background-color: ${props => {
-    switch (props.tile) {
-      case 'wall':
-        return 'black'
-      case 'ground':
-        return 'green'
-      case 'rock':
-        return 'grey'
-      case 'tree':
-        return 'darkgreen'
-      default:
-        break;
-    }
-  }};
-    color: ${props => {
-    switch (props.tile) {
-      case 'wall':
-        return 'black'
-      case 'ground':
-        return 'green'
-      case 'rock':
-        return 'grey'
-      case 'tree':
-        return 'darkgreen'
-      default:
-        break;
-    }
-  }};
-  &:hover{
-    opacity: .8;
-  }
-`
-const Entity = styled.div`
-  font-size: 40px;
-  height: 50px;
-  width: 50px;
-  color: ${props => {
-    switch (props.tile) {
-      case 'player':
-        return 'pink'
-      case 'apple':
-        return 'red'
-      default:
-        break;
-    }
-  }};
-  &:hover{
-    opacity: .8;
-  }
-`
 
 class App extends Component {
   state = {
@@ -72,7 +14,6 @@ class App extends Component {
   componentDidMount() {
     this.setState({ editTiles: tileGenerator(120) })
     document.addEventListener('DOMContentLoaded', () => {
-      'use strict';
       document.addEventListener('keydown', event => {
         this.fireKey(event)
       });
---
+++ src/components/styled.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const TileContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 50px);
+`
+
+export const Tile = styled.div`
+  height: 50px;
+  width: 50px;
+  background-color: ${props => {
+    switch (props.tile) {
+      case 'wall':
+        return 'black'
+      case 'ground':
+        return 'green'
+      case 'rock':
+        return 'grey'
+      case 'tree':
+        return 'darkgreen'
+      default:
+        break;
+    }
+  }};
+    color: ${props => {
+    switch (props.tile) {
+      case 'wall':
+        return 'black'
+      case 'ground':
+        return 'green'
+      case 'rock':
+        return 'grey'
+      case 'tree':
+        return 'darkgreen'
+      default:
+        break;
+    }
+  }};
+  &:hover{
+    opacity: .8;
+  }
+`
+export const Entity = styled.div`
+  font-size: 40px;
+  height: 50px;
+  width: 50px;
+  color: ${props => {
+    switch (props.tile) {
+      case 'player':
+        return 'pink'
+      case 'apple':
+        return 'red'
+      default:
+        break;
+    }
+  }};
+  &:hover{
+    opacity: .8;
+  }
+`
