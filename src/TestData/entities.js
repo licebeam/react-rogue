76dc57ceaca19a80bd2b089f89507d1b6e55a4f5
licebeam <dustinwalkerart@gmail.com>
2019-06-12 15:00:37 +0900

    removes entities

--- src/TestData/entities.js
+++ src/TestData/entities.js
@@ -2,9 +2,4 @@ import { MAX_WORLD_WIDTH, MAX_WORLD_HEIGHT } from '../constants/constants';
 export const entities = [
   { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2), type: 'player', char: '@', img: null },
   { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 2, type: 'rat', char: 'o', img: null },
-  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 3, type: 'rat', char: 'o', img: null },
-  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 4, type: 'rat', char: 'o', img: null },
-  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 1, type: 'rat', char: 'o', img: null },
-  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 5, type: 'rat', char: 'o', img: null },
-  { id: (MAX_WORLD_HEIGHT / 2) + (MAX_WORLD_WIDTH / 2) + 6, type: 'rat', char: 'o', img: null },
 ]
