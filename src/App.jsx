3dc5fd0e494aacf8e4d9512b4863047efc11cd8e
licebeam <dustinwalkerart@gmail.com>
2019-06-12 15:33:00 +0900

    fixes entity generation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { entities } from './TestData/entities';
 import { TileContainer, Tile } from './components/styled';
 import { tileGenerator } from './helpers/tileFunctions';
 import { changePlayerPosition } from './helpers/moveFunctions';
+import { flatten } from 'lodash';
 
 class App extends Component {
   state = {
@@ -11,6 +12,7 @@ class App extends Component {
     currentRoom: [],
     currentRoomId: 0,
     allRooms: [],
+    allEntities: [],
     loading: false,
   }
 
@@ -46,14 +48,21 @@ class App extends Component {
   generateRooms = (roomSize, roomAmount) => {
     const { editEntities } = this.state;
     var rooms = []
+    var entities = []
     var i = 0;
     const currentFloorStairLoc = rooms.length && rooms[i].find(t => t.tile.name === 'portal').id || null;
     const currentFloorPlayerLoc = editEntities.find(en => en.type === 'player').id || null
-    console.log(currentFloorPlayerLoc, currentFloorStairLoc)
     for (i = 0; i < roomAmount; i++) {
       rooms.push({ roomId: i, room: tileGenerator(roomSize, currentFloorStairLoc, currentFloorPlayerLoc) });
     }
-    return this.setState({ allRooms: rooms })
+    return this.setState({ allRooms: rooms }, () => {
+      //Generate All Entities for floor.
+      var k = 0;
+      for (k = 0; k < roomAmount; k++) {
+        entities.push(this.entityGenerator(this.state.allRooms[k].room, k));
+      }
+      this.setState({ allEntities: flatten(entities) })
+    })
   }
 
   //PLAYER KEYS
@@ -74,11 +83,25 @@ class App extends Component {
     }
   }
 
+  entityGenerator = (curRoom, roomId) => {
+    const entities = curRoom.map(t => {
+      let randomTile = Math.floor(Math.random() * (MAX_WORLD_HEIGHT - 1 + 1)) + 1;
+      if (t.tile.name !== 'wall') {
+        if (t.id === (randomTile + 1) || (t.id === (randomTile - 1) || t.id === randomTile)) {
+          return { roomId, id: t.id, entity: { type: 'rat', char: 'o', img: null } } // change to object
+        } else return null
+      } else return null
+    })
+    const flattenedEntities = entities.filter(e => e)
+    return flattenedEntities;
+  }
+
   produceEntityOnScreen = tileId => {
-    const { editEntities } = this.state;
-    const sentEntity = editEntities.find(ent => {
-      if (ent.id === tileId) {
-        return ent
+    const { allEntities, currentRoomId } = this.state;
+    console.log(allEntities)
+    const sentEntity = allEntities.find(ent => {
+      if (ent.id === tileId && currentRoomId === ent.roomId) {
+        return ent.entity
       }
     })
     if (sentEntity)
@@ -96,11 +119,11 @@ class App extends Component {
             if (this.produceEntityOnScreen(t.id)) {
               const ent = this.produceEntityOnScreen(t.id)
               return (
-                <Tile tile={ent.type} key={ent.id}>
+                <Tile tile={ent.entity.type} key={ent.id}>
                   {/* conditionally render image */}
-                  {ent.img ? (
-                    <img className='sprite-image' src={ent.img} alt="" />
-                  ) : ent.char}
+                  {ent.entity.img ? (
+                    <img className='sprite-image' src={ent.entity.img} alt="" />
+                  ) : ent.entity.char}
                 </Tile>
               )
             }
